5b9966e933d222897b6b877620837ea80112cf36
KAZUSA <knaoi@student.holmesglen.edu.au>
2023-11-28 17:20:10 +1100

    add tkinter

--- PracticalTask3/Catalog.py
+++ PracticalTask3/Catalog.py
@@ -1,33 +1,55 @@
-import sys
+import tkinter as tk
 from Book import Book
 
-def main():
-    catalog = [
-        Book("0553296981", "The Diary of a Young Girl", "Anne Frank", 16.50),
-        Book("1400082773", "Dreams from My Father", "Barack Obama", 24.99)
-    ]
+# スタート時にカタログに本を追加します
+catalog = [
+    Book("123456789", "Book A", "Author A", 9.99),
+    Book("987654321", "Book B", "Author B", 14.99),
+    # その他の本を追加...
+]
 
-    while True:
-        print_menu()
-        option = input("Enter option number: ")
-        if option == "1":
-            add_book(catalog)
-        elif option == "2":
-            display_sorted(catalog)
-        elif option == "3":
-            search_books(catalog)
-        elif option == "4":
-            delete_book(catalog)
-        elif option == "5":
-            display_books(catalog)
-        elif option == "6":
-            if confirm_exit():
-                print("Exiting the program.")
-                sys.exit()
-        else:
-            print("Invalid option, please try again.")
+# GUIの構築に必要な関数を追加します
+def add_book():
+    pass  # 本を追加する関数
 
-# Function definitions for add_book, display_sorted, search_books, delete_book, display_books, print_menu, confirm_exit...
+def sort_and_display():
+    pass  # 本を価格でソートし表示する関数
 
-if __name__ == "__main__":
-    main()
+def search_books():
+    pass  # タイトルで本を検索する関数
+
+def delete_book():
+    pass  # 本を削除する関数
+
+def display_all_books():
+    pass  # すべての本を表示する関数
+
+def exit_program():
+    root.destroy()  # GUIウィンドウを閉じてプログラムを終了
+
+# tkinter ウィンドウを作成
+root = tk.Tk()
+root.title("Book Catalog")
+
+# メニューボタンを作成
+button_frame = tk.Frame(root)
+button_frame.pack(side=tk.TOP, pady=(10, 0))
+
+tk.Button(button_frame, text="1. Add a Book", command=add_book).pack(fill=tk.X)
+tk.Button(button_frame, text="2. Sort and Display the Books by Price", command=sort_and_display).pack(fill=tk.X)
+tk.Button(button_frame, text="3. Search Books by Title", command=search_books).pack(fill=tk.X)
+tk.Button(button_frame, text="4. Delete a Book", command=delete_book).pack(fill=tk.X)
+tk.Button(button_frame, text="5. Display All Books", command=display_all_books).pack(fill=tk.X)
+tk.Button(button_frame, text="6. Exit", command=exit_program).pack(fill=tk.X)
+
+# ユーザーの入力用エリア
+entry_frame = tk.Frame(root)
+entry_frame.pack(side=tk.TOP, pady=(5, 10))
+
+entry_label = tk.Label(entry_frame, text="Enter menu choice:")
+entry_label.pack(side=tk.LEFT)
+entry = tk.Entry(entry_frame)
+entry.pack(side=tk.LEFT, expand=True, fill=tk.X)
+
+# GUIを実行
+root.mainloop()
